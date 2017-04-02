f1954b6410b5fb70d11cdbc78037e2dc0e77ab2a
Eugene Belov <eugene.belov@smashdocs.net>
2017-04-02 23:51:44 +0300

    add event handlers to doc item

--- sidebar-app/src/app/sidebar/sidebar.view.ts
+++ sidebar-app/src/app/sidebar/sidebar.view.ts
@@ -7,13 +7,20 @@ import { Component } from '@angular/core';
 export class SidebarView {
   name = 'OMG';
   documents = [{
+    id: "1",
     type: "draft",
     title: "some title 1"
   }, {
+    id: "2",
     type: "review",
     title: "some title 2"
   }, {
+    id: "3",
     type: "draft",
     title: "some title 3"
-  }]
+  }];
+
+  showChanges(itemId) {
+    console.log("show changes", itemId);
+  }
 }
